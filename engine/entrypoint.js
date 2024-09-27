63b58965a58ab38d675a9876b170ab4e1916fb34
Ben Celsi <benjamin.d.celsi@gmail.com>
2024-09-27 16:33:49 -0700

    sc

--- engine/entrypoint.js
+++ engine/entrypoint.js
@@ -1,24 +1,21 @@
 // hacky solution to get around js module / CORS shenanigans locally
 
-// find the game folder, "game" if none provided
-let gameName = window.location.search === "" ? "game" : window.location.search.substring(1).split('&')[0]
-console.log(gameName)
-const GAME_FOLDER = "../games/" + gameName
-
-function wait(duration, then) {
-	setTimeout(() => { then() }, duration)
-}
+// find the game folder
+const GAME_FOLDER = "../games/" + window.location.search.substring(1).split('&')[0]
 
 // first, load the game data
 // then, load the engine which wil run the game
 addScript(GAME_FOLDER + "/game.js")
 addScript("engine.js")
 
-function addScript(src, onload=null) {
-    console.log(src)
+function addScript(src) {
     let script = document.createElement("script")
     script.type = "text/javascript"
     script.src = src
-    if (onload != null) { script.onload = onload }
     document.head.appendChild(script);
+}
+
+// shared functions
+function wait(duration, then) {
+	setTimeout(() => { then() }, duration)
 }
