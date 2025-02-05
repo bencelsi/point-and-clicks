39e9f071ce14cf132a19c677357beab6298080ca
Ben Celsi <benjamin.d.celsi@gmail.com>
2025-02-05 12:53:42 -0800

    sc

--- engine/start.js
+++ engine/start.js
@@ -1,11 +1,12 @@
+
 // hacky solution to get around js module / CORS shenanigans locally
-const GAME_NAME = window.location.search.substring(1).split('&')[0]
+const GAME_NAME = new URLSearchParams(window.location.search).get('game');
 const GAME_PATH = "../games/" + GAME_NAME
+
 
 // first, load the game data
 addScript(GAME_PATH + "/" + GAME_NAME + ".js")
 
-// addStyle("baseStyle.css")
 addStyle(GAME_PATH + "/" + GAME_NAME + ".css")
 
 // then, load the engine which wil run the game
